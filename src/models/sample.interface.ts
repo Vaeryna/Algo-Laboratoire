81e4cae22faf4da41761452b331dbd562ec2ad3e
Melanie <leblond.melanie@gmail.com>
2026-03-03 14:42:02 +0100

    function sort by priority

--- src/models/sample.interface.ts
+++ src/models/sample.interface.ts
@@ -1,7 +1,9 @@
+import {Priority} from "./priority.enums";
+
 export interface Sample {
     "id": string,
     "type": string,
-    "priority": string,
+    "priority": Priority,
     "analysisTime": number,  //durée en minutes
     "arrivalTime": string,          // heure en HH:MM
     "patientId": string
