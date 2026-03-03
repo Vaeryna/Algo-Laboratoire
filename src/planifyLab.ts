013758c00d905cd1eaa18a489443647f098b7f25
Melanie <leblond.melanie@gmail.com>
2026-03-03 13:47:12 +0100

    call create planning service

--- src/planifyLab.ts
+++ src/planifyLab.ts
@@ -1,5 +1,18 @@
+import {createPlanning} from "./services/createPlanning.service";
+import {INPUT_PATH} from "./config";
+import {Input} from "./models/input.models";
+import {readFileSync} from "fs";
+
+function loadInput(path: string): Input {
+    const raw = readFileSync(path, "utf-8");
+    return JSON.parse(raw)
+}
+
+const input = loadInput(INPUT_PATH)
+
+
 function planifyLab() {
-    console.log("pouet")
+    console.log("index", createPlanning(input))
 }
 
 
