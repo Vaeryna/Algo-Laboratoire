b8a725c1648c6be93e09d823c83c2efa37a7be8d
Melanie <leblond.melanie@gmail.com>
2026-03-03 16:16:19 +0100

    add daily schedule to each technician

--- src/services/assignation.service.ts
+++ src/services/assignation.service.ts
@@ -23,13 +23,18 @@ export function assignation(input: Input) {
 //je recupere tout le fichier : samples et techniciens et machines
 
     const samples: Sample[] = input.samples;
-    const technicians: Technician[] = input.technicians.map(tech => ({
-        ...tech, availableTime: []
-    }));  //ajout statut à la volée
+    const technicians: Technician[] = input.technicians.map(tech => {
+                let program = createProgram(tech);
+                return ({...tech, program})
+            }
+        )
+
+
+    ;  //ajout statut à la volée
     const equipment: Equipment[] = input.equipment;
 
-    // console.log(technicians)
+console.log("techos", technicians)
 
-    createProgram(technicians[0])
+
 
 }
