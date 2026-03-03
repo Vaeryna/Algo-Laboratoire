3f26a28d9670dff434dd2260d2f9a8105b97913e
Melanie <leblond.melanie@gmail.com>
2026-03-03 13:46:37 +0100

    add output interface

---
+++ src/models/output.interface.ts
@@ -0,0 +1,17 @@
+export interface Output {
+    "schedule": [
+        {
+            "sampleId": string,
+            "technicianId": string,
+            "equipmentId": string,
+            "startTime": string
+            "endTime": string,
+            "priority": string
+        }
+    ],
+    "metrics": {
+        "totalTime": number,      // Une seule analyse de 30min
+        "efficiency": number,  // Ressources utilisées à 100%
+        "conflicts": number        // Aucun conflit
+    }
+}
