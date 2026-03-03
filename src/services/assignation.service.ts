c18b0dd4342995849f26f5e5403fa2c52868ca50
Melanie <leblond.melanie@gmail.com>
2026-03-03 16:10:42 +0100

    create Daily Program ok

---
+++ src/services/assignation.service.ts
@@ -0,0 +1,35 @@
+import {Sample} from "../models/sample.interface";
+import {Technician} from "../models/technician.interface";
+import {Input} from "../models/input.models";
+import {Equipment} from "../models/equipment.interface";
+import {transformToMinutes} from "../utils/transformToMinutes.utils";
+
+
+function createProgram(technician: Technician): Record<string, boolean> {
+    const startTime: number = transformToMinutes(technician.startTime)
+    const endTime: number = transformToMinutes(technician.endTime)
+
+    let program: Record<number, boolean> = {}
+
+    for (let i = startTime; i < endTime; i += 15) {
+        program[i] = true
+    }
+
+    return program
+}
+
+
+export function assignation(input: Input) {
+//je recupere tout le fichier : samples et techniciens et machines
+
+    const samples: Sample[] = input.samples;
+    const technicians: Technician[] = input.technicians.map(tech => ({
+        ...tech, availableTime: []
+    }));  //ajout statut à la volée
+    const equipment: Equipment[] = input.equipment;
+
+    // console.log(technicians)
+
+    createProgram(technicians[0])
+
+}
