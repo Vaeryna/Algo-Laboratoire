c131f8b89e0993055661f5e4e8d5a02ad8d8d58c
Melanie <leblond.melanie@gmail.com>
2026-03-04 18:11:25 +0100

    add write output in txt file (in data folder)

--- src/planifyLab.ts
+++ src/planifyLab.ts
@@ -2,6 +2,7 @@ import {INPUT_PATH} from "./config";
 import {Input} from "./models/input.models";
 import {readFileSync} from "fs";
 import {createPlanning} from "./services/createOutPutFile";
+import * as fs from "node:fs";
 
 function loadInput(path: string): Input {
     const raw = readFileSync(path, "utf-8");
@@ -13,6 +14,8 @@ const input = loadInput(INPUT_PATH)
 
 function planifyLab() {
     console.log(createPlanning(input))
+
+    fs.writeFileSync("src/data/output.txt", JSON.stringify(createPlanning(input), null, 2))
 
 }
 
