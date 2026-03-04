580d74df1e9a207a363f56cb1709e9783ebe5820
Melanie <leblond.melanie@gmail.com>
2026-03-04 17:09:41 +0100

    launch application

--- src/planifyLab.ts
+++ src/planifyLab.ts
@@ -1,7 +1,7 @@
-import {createPlanning} from "./services/createPlanning.service";
 import {INPUT_PATH} from "./config";
 import {Input} from "./models/input.models";
 import {readFileSync} from "fs";
+import {createPlanning} from "./services/createOutPutFile";
 
 function loadInput(path: string): Input {
     const raw = readFileSync(path, "utf-8");
@@ -12,7 +12,7 @@ const input = loadInput(INPUT_PATH)
 
 
 function planifyLab() {
-    console.log("index", createPlanning(input))
+    createPlanning(input)
 }
 
 
