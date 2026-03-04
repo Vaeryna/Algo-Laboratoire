90d9ee55e90e2f8a0783872fd53db39b61b33a72
Melanie <leblond.melanie@gmail.com>
2026-03-04 15:17:23 +0100

    sorting sample by priority & arrival time

--- src/services/assignation.service.ts
+++ src/services/assignation.service.ts
@@ -2,7 +2,9 @@ import {Sample} from "../models/sample.interface";
 import {Technician} from "../models/technician.interface";
 import {Input} from "../models/input.models";
 import {Equipment} from "../models/equipment.interface";
-import {transformToMinutes} from "../utils/transformToMinutes.utils";
+import {transformToMinutes} from "../utils/transformTime.utils";
+import {sortByPriority} from "../utils/sortBy.utils";
+import {booking, findAllTechAvailableBySpe} from "../utils/availability.utils";
 
 
 function createProgram(userPlanning?: Technician | Equipment): Record<number, string> {
@@ -22,6 +24,13 @@ function createProgram(userPlanning?: Technician | Equipment): Record<number, st
     }
     return program
 }
+
+const sortSamples: (samples: Sample[]) => Sample[] = (samples: Sample[]): Sample[] => sortByPriority(samples) || sortByPriority(samples);
+
+const addEndTime: (samples: Sample[]) => Sample[] = (samples: Sample[]): Sample[] => samples.map(sample => {
+    let totalTime: number = transformToMinutes(sample.arrivalTime) + sample.analysisTime;
+    return ({...sample, endTime: totalTime})
+});
 
 
 export function assignation(input: Input) {
--- src/utils/sortBy.utils.ts
+++ src/utils/sortBy.utils.ts
@@ -1,21 +1,30 @@
-import {Input} from "../models/input.models";
 import {Priority, priorityOrder} from "../models/priority.enums";
-import {transformToMinutes} from "./transformToMinutes.utils";
+import {transformToMinutes} from "./transformTime.utils";
+import {Sample} from "../models/sample.interface";
 
-export function sortByPriority(input: Input) {
+export function sortByPriority(input: Sample[]) {
 
-    return input.samples.sort((a, b): number => {
+    return input.sort((a, b): number => {
         if (!a.priority) a.priority = Priority.ROUTINE
         if (!b.priority) b.priority = Priority.ROUTINE
 
         return priorityOrder[a.priority] - priorityOrder[b.priority]
-
     })
 }
 
-export function sortByArrivalTime(input: Input) {
-    return input.samples.sort((a, b): number => {
+export function sortSampleByArrivalTime(input: Sample[]): Sample[] {
+    return input.sort((a, b): number => {
         return transformToMinutes(a.arrivalTime) - transformToMinutes(b.arrivalTime)
     })
+}
 
+export function sortByEndTimeSample(input: Sample[]): Sample[] {
+    const totalTime = input.map(sample => {
+        let totalTime: number = transformToMinutes(sample.arrivalTime) + sample.analysisTime;
+        return ({...sample, endTime: totalTime})
+    })
+
+    return totalTime.sort((a, b): number => {
+        return a.endTime - b.endTime
+    })
 }
