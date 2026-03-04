b8737168662d1ec8bb12dba9d189f6e7df671376
Melanie <leblond.melanie@gmail.com>
2026-03-04 15:18:24 +0100

    add facultative key "program" for technician daily planning

--- src/models/technician.interface.ts
+++ src/models/technician.interface.ts
@@ -3,7 +3,8 @@ export interface Technician {
     "id": string,
     "name": string,
     "speciality": string,
-    "startTime": string,            // Début de service | heure en HH:MM
-    "endTime": string            // Fin de service | heure en HH:MM
+    "startTime": string,
+    "endTime": string,
+    "program"?: Record<number, string>
 
 }
