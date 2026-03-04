b93490cc62c83309b5be8fd2517faf49817ad661
Melanie <leblond.melanie@gmail.com>
2026-03-04 17:00:35 +0100

    clean unused function + add function sort by technician speciality

--- src/utils/sortBy.utils.ts
+++ src/utils/sortBy.utils.ts
@@ -1,9 +1,9 @@
 import {Priority, priorityOrder} from "../models/priority.enums";
 import {transformToMinutes} from "./transformTime.utils";
 import {Sample} from "../models/sample.interface";
+import {Technician} from "../models/technician.interface";
 
 export function sortByPriority(input: Sample[]) {
-
     return input.sort((a, b): number => {
         if (!a.priority) a.priority = Priority.ROUTINE
         if (!b.priority) b.priority = Priority.ROUTINE
@@ -18,13 +18,16 @@ export function sortSampleByArrivalTime(input: Sample[]): Sample[] {
     })
 }
 
-export function sortByEndTimeSample(input: Sample[]): Sample[] {
-    const totalTime = input.map(sample => {
-        let totalTime: number = transformToMinutes(sample.arrivalTime) + sample.analysisTime;
-        return ({...sample, endTime: totalTime})
-    })
 
-    return totalTime.sort((a, b): number => {
-        return a.endTime - b.endTime
+export function sortByTechSpe(input: Technician[], speciality: string) {
+    const GENERAL = "GENERAL"
+
+    return [...input].sort((a, b): number => {
+        const score = (t: Technician) => {
+            if (t.speciality === speciality) return 0
+            if (t.speciality === GENERAL) return 1
+            return 2
+        }
+        return score(a) - score(b)
     })
 }
